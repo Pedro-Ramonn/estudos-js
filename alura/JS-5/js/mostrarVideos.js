ada72e59e623c27b0b5f12f0365990b74bb0aa79
Pedro Ramon <pedrorcdiaz5@gmail.com>
2024-08-01 20:33:38 -0300

    aula 1 e 2 de js-5

---
+++ alura/JS-5/js/mostrarVideos.js
@@ -0,0 +1,45 @@
+import { conectaApi } from "./consumoApi.js";
+
+const lista = document.querySelector("[data-lista]");
+
+function constroiCard(titulo, descricao, url, imagem) {
+    const video = document.createElement("li");
+    video.className = "videos__item";
+    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+<div class="descricao-video">
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
+</div>`
+
+    return video;
+}
+
+async function listaVideos() {
+    const listaApi = await conectaApi.listaVideos();
+    listaApi.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+}
+
+/* async function jeitoCoreto() {
+    const x = await conectaApi.listaVideos();
+    x.forEach((video) => {
+        lista.innerHTML +=
+        `
+        <iframe width="100%" height="72%" src="${video.url}"
+            title="${video.titulo}" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+        <div class="descricao-video">
+            <img src="${video.imagem}" alt="logo canal alura">
+            <h3>${video.titulo}</h3>
+            <p>${video.descricao}</p>
+        </div>`
+        ;
+    })
+    } */
+
+    listaVideos()
